11c9c8e0bd424242ff913cb53550a068cbe44a46
Tinku Lalwani <tinkulalwani@gmail.com>
2022-07-31 17:02:17 +0530

    Authentication

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require("express");
+const passport = require("passport");
+const { googleClientID, googleClientSecret } = require("./config/keys");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require('./config/keys');
+
+
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send(
-        {
-            Hello: "Buddy",
-            Bye: "Dude",
-            Deta: process.env.PORT
-        }
-    );
-});
+// app.get('/', (req,res) => {
+//     res.send(
+//         {
+//             Hello: "Buddy",
+//             Bye: "Dude",
+//         }
+//     );
+// });
 
-const PORT = process.env.PORT || 4000;
+passport.use(new GoogleStrategy(
+    {
+        clientID: googleClientID,
+        clientSecret: googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, () => {
+        console.log("Access Control", accessToken)
+    }
+));
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google')
+);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
