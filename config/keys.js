9dfe5718ff8c18562ccd9fe8e60b4dea93c74d0f
Tinku Lalwani <tinkulalwani@gmail.com>
2022-08-31 18:38:50 +0530

    Added a console message

--- config/keys.js
+++ config/keys.js
@@ -2,9 +2,10 @@
 
 if(process.env.NODE_ENV === 'production') {
     //Production environment uses key.js
+    console.log("Production Executed...");
     module.exports = require("./prod");
-
 } else {
     //Development environment uses dev.js
+    console.log("Development Executed...");
     module.exports = require("./dev");
 }
